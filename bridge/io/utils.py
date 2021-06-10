2d6c2e7dfc22996a06c77048754498b907f314d2
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-06-10 12:53:51 -0700

    add gitignore; input cleaning + subdirectory creation for feff inp generator

--- bridge/io/utils.py
+++ bridge/io/utils.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pathlib
 from pymatgen.core import Structure, Molecule, Lattice
+from pymatgen.core.periodic_table import Element
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.io.feff.outputs import LDos
 from pymatgen.io.feff.inputs import Atoms, Header, Potential, Tags
-from atomate.feff.workflows.core import get_unique_site_indices
 
 def poscar_to_xyz(fp, output="structure.xyz"):
     atoms_pos = Poscar.from_file(fp)
@@ -18,12 +19,23 @@ def poscar_to_mol(fp):
 
 def create_feff_inp(atoms, target_species=None, target_dir="", **kwargs):
     # 3 options -> if atoms is molecule object; if atoms is poscar file; 
+
+
     if isinstance(atoms, Molecule):
         mol = atoms
     elif (atoms.split("/")[-1] == "POSCAR"):
         mol = poscar_to_mol(atoms)
     elif (atoms.split(".")[-1] == "xyz"):
         mol = Molecule.from_file(atoms)
+
+    if target_species is None:
+        target_species = mol.species[0]
+    else:
+        if not isinstance(target_species, Element):
+            try:
+                target_species = Element(target_species)
+            except ValueError:
+                print("Could not convert target species to element class.")
 
     # need a structure only to make header file so pymatgen can leader read the feff output
     lvecs = np.max(mol.cart_coords, axis=0)
@@ -34,9 +46,9 @@ def create_feff_inp(atoms, target_species=None, target_dir="", **kwargs):
             "SCF": "7 0 30 0.2 3",
             "FMS": "9 0",
             "LDOS": "-30.0 30.0 0.1",
-            "RECIPROCAL": "",
             "EDGE": "K",
             "COREHOLE": "RPA",
+            "XANES":""
             }
 
     for key in kwargs:
@@ -45,18 +57,23 @@ def create_feff_inp(atoms, target_species=None, target_dir="", **kwargs):
     fheader = Header(struct)
     ftags = Tags(tags)
 
-    # TODO: reduce over unique site indices
-    # then make subdirectory in target_dir for each site 
+    # TODO: reduce over unique site indices-- is there a pymatgen routine for molecule site equivalence?
+    # then make subdirectory in target_dir for each site, named "target_Z_site_N_edge_K"
+    ind = [i for i, j in enumerate(mol.species) if j is target_species]
 
-    fpot = Potential(mol, absorbing_atom=0)
-    fatoms = Atoms(mol, absorbing_atom=0, radius=50)
+    for i, j in enumerate(ind):
+        fpot = Potential(mol, absorbing_atom=j)
+        fatoms = Atoms(mol, absorbing_atom=j, radius=50)
 
-    out_str = fheader.__str__() + "\n\n" \
-                + ftags.__str__() + "\n\n" \
-                + fpot.__str__() + "\n\n" \
-                + fatoms.__str__()
-            
+        out_str = fheader.__str__() + "\n\n" \
+                    + ftags.__str__() + "\n\n" \
+                    + fpot.__str__() + "\n\n" \
+                    + fatoms.__str__()
 
-    with open(target_dir+"feff.inp", "w+") as f:
-        f.write(out_str)
-        f.close()
+        out_dir = target_dir + "target_" + target_species.name \
+                    + "_site_" + str(i) + "_edge_" + tags["EDGE"] + "/"
+                
+        pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+        with open(out_dir+"feff.inp", "w+") as f:
+            f.write(out_str)
+            f.close()
